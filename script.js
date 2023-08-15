59e922e027db2dd32135ca0be1fd3bc3e6302962
JulienSLuk <s10219568@connect.np.edu.sg>
2023-08-15 14:46:24 +0800

    hover animations, background gradients, and scroll-triggered animations

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// You can add any JavaScript interactions here if needed
+// Add animations and interactions using JavaScript
+// Example: Scroll-triggered animations using the Intersection Observer API
+const sections = document.querySelectorAll('section');
+
+const sectionObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+sections.forEach(section => {
+  sectionObserver.observe(section);
+});

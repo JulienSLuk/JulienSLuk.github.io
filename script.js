824837249d92c1c4b735f77e36f80b6368285654
JulienSLuk <s10219568@connect.np.edu.sg>
2023-08-15 16:08:26 +0800

    refactored code

--- script.js
+++ script.js
@@ -109,22 +109,6 @@ particlesJS('particles-js', {
   retina_detect: true
 });
 
-// Add animations and interactions using JavaScript
-// Example: Scroll-triggered animations using the Intersection Observer API
-const sections = document.querySelectorAll('section');
-
-const sectionObserver = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-      observer.unobserve(entry.target);
-    }
-  });
-}, { threshold: 0.5 });
-
-sections.forEach(section => {
-  sectionObserver.observe(section);
-});
 
 
 // Fetch user's IP address and display greeting
@@ -142,3 +126,21 @@ async function fetchUserIP() {
 
 // Call the function to fetch user's IP
 fetchUserIP();
+
+// Add animations and interactions using JavaScript
+// Example: Scroll-triggered animations using the Intersection Observer API
+const sections = document.querySelectorAll('section');
+
+const sectionObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+sections.forEach(section => {
+  sectionObserver.observe(section);
+});
+
